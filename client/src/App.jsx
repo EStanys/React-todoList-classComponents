8383f585da6217a62ad8ad725189dd90edcfcee5
Edvinas <edvinasstanys@gmail.com>
2021-06-15 11:38:14 +0300

    redirect works, pages moved to src

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { Route, Switch } from 'react-router'
+import { Route, Switch, Redirect } from 'react-router'
 import TodoPage from './components/todoPage/todoPage'
 import NavBar from './components/navBar/navBar'
-import ContactsPage from './components/pages/contacts'
-import AboutPage from './components/pages/about'
-import HomePage from './components/pages/home'
+import ContactsPage from './pages/contacts'
+import AboutPage from './pages/about'
+import HomePage from './pages/home'
+import SingleTodo from './pages/singleTodo'
+import NotFound from './pages/404'
 
 // app styles
 import classes from './app.module.css';
@@ -17,10 +19,14 @@ class App extends Component{
         <NavBar />
         <div className={classes.container}>
           <Switch>
+            <Route path="/todos/:id" component={SingleTodo}></Route>
             <Route path="/todos" component={TodoPage}></Route>
             <Route path="/about" component={AboutPage}></Route>
             <Route path="/contacts" component={ContactsPage}></Route>
-            <Route path="/" component={HomePage}></Route>
+            <Route path="/" exact component={HomePage}></Route>
+            <Route path="/not-found" exact component={NotFound}></Route>
+            <Redirect from="/buble" to="/todos"></Redirect>
+            <Redirect to="/not-found"></Redirect>
           </Switch>
 
           {/* <Route path="/" exact component={HomePage}></Route> */}
