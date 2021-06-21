21018e8a6a55bbb54ded9a5c067e11c59c72f624
Edvinas <edvinasstanys@gmail.com>
2021-06-21 09:43:16 +0300

    all g

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'font-awesome/css/font-awesome.css'
 
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
